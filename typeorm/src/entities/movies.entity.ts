1324d1660b860a60a53f848fafdb2feb8ea58c41
Thiago Barcelos <thiagolbf@hotmail.com>
2023-07-24 21:30:25 -0300

    create movie table with migration

--- typeorm/src/entities/movies.entity.ts
+++ typeorm/src/entities/movies.entity.ts
@@ -1,3 +1,23 @@
-class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn, Check } from "typeorm";
+
+@Entity("movies")
+class Movie {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ length: 50, unique: true })
+  name: string;
+
+  @Column({ type: "text", nullable: true })
+  description?: string | undefined | null;
+
+  @Column()
+  @Check("duration > 0")
+  duration: number;
+
+  @Column()
+  @Check("price > 0")
+  price: number;
+}
 
 export default Movie;
